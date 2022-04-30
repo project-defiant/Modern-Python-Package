19b131762877fb76d764d9a5c96f544696204a17
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2022-04-30 20:39:32 +0200

    python package structure

---
+++ Package/package.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+def main() -> None:
+    """Main dummy function
+
+    Returns:
+        str: msg about the main invocation
+    """
+    return f"package {__name__} function has been invoked"
